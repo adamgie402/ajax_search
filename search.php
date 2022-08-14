7297bf208137c6a4844705d29192e8f583077fca
adamgie402 <adamgie402@gmail.com>
2022-08-14 18:24:20 +0200

    php search file created

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+$customer[] = "Adam";
+$customer[] = "Andrzej";
+$customer[] = "Alfons";
+$customer[] = "Marek";
+$customer[] = "Marcin";
+$customer[] = "Maciej";
+$customer[] = "Sonia";
+$customer[] = "Beata";
+$customer[] = "Jadwiga";
+$customer[] = "Katarzyna";
+$customer[] = "Mariusz";
+$customer[] = "Sabina";
+$customer[] = "Sylwia";
+
+$response = '';
+$query = htmlspecialchars(trim($_REQUEST['query'])); // zapytanie przekazane metodą GET
+
+?>
